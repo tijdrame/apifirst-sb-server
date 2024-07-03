8ad1e399bccf171da104ea6630d9f3b5c1af3b3c
Tidiane <tijdrame@gmail.com>
2024-07-02 20:39:21 -0400

    finaliser multimodule

---
+++ apifirst-server/src/test/java/com/emard/apifirst/ApifirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emard.apifirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
